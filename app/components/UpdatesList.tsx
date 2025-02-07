e3e7f56222e5762c987b7305ac7da3627f3b2ca2
Win <131394482+win-ne@users.noreply.github.com>
2025-02-07 10:10:59 +0300

    feat: fetch updates from bcms

--- app/components/UpdatesList.tsx
+++ app/components/UpdatesList.tsx
@@ -10,22 +10,25 @@ const UpdateList = () => {
     useEffect(() => {
         fetchUpdates().then(upts => {
             setUpdates(upts)
-            console.log(upts)
         })
     }, [])
 
     return <div className="flex flex-col max-w-[600px] items-center gap-4">
         <h1 className="text-3xl mt-4 max-w-[600px]">Updates</h1>
         <p className="text-gray-500 max-w-[400px]">Join the waitlist to get early access of the product and recieve updates on the progress!</p>
-        {/* {[].map((item, index) => {
+        {updates.map((item, index) => {
             return <div key={`update-${index}`} className="bg-white/10 border border-white/15 p-4 rounded-2xl mb-2 text-start">
                 <span className="flex w-100 justify-between">
-                    <p className="text-orange-500">Title</p>
-                    <p className="text-orange-500">Date</p>
+                    <p className="text-orange-500">{item.meta.en?.title}</p>
+                    <p className="text-orange-500 text-xs">{(new Date(item.updatedAt)).toLocaleDateString()}</p>
                 </span>
-                <span className="text-gray-300">{item}</span>
+                <span className="text-gray-300">
+                    {item.meta.en?.body.nodes.map((node, index) => (
+                        <div key={index} dangerouslySetInnerHTML={{ __html: node.value }} />
+                    ))}
+                </span>
             </div>
-        })} */}
+        })}
     </div>
 }
 
